4e4a875761685818ea1332d157a11e2b574ef379
hristo2612 <hristoapps@gmail.com>
2018-05-24 23:27:19 +0300

    added: backend logic for handling Github login, by sending auth token

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 5000;
 const clientFolder = '../client/build';
@@ -25,11 +28,36 @@ if (cluster.isMaster) {
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, clientFolder)));
 
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
   // Answer API requests.
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
+
+
+  app.post('/api/login', function (req, res) {
+    axios.post('https://github.com/login/oauth/access_token', { client_id: '3145d9a7608514f31567', client_secret: 'Your_Client_Secret', code: req.body.code, accept: 'json' })
+    .then(result => {
+      var access = (result.data).match(/access_token=[A-Za-z0-9]+=*&/);
+      access = access[0];
+      access = access.replace("access_token=", "");
+      access = access.slice(0, -1);
+      console.log(access);
+      res.json({"success": true, "tkn": access});
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({"success": false, "error": "Something went wrong while trying to authenticate"});
+    });
+    //res.send('Hello World!');
+    //res.send('{"message":"Hello from the custom server!"}');
+  });
+
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
