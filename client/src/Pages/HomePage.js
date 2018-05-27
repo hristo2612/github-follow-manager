11f17670b9b0b2ba72ebd6021a5413938d3e8f37
hristo2612 <hristoapps@gmail.com>
2018-05-27 22:35:22 +0300

    refactor: HomePage - add  github-login functionality

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -8,6 +8,7 @@ import ordinal from 'ordinal';
 import Dialog from '../Generic/Dialog';
 import List from '../Generic/List';
 import Button from '@material-ui/core/Button';
+import GitHubLogin from 'react-github-login';
 
 import '../App.css';
 
@@ -25,13 +26,8 @@ const rightListStyle = {
 class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
-    //this.createRows();
-    this._columns = [
-      { key: 'id', name: 'Place' },
-      { key: 'avatar', name: 'Avatar', formatter: ImageFormatter },
-      { key: 'user', name: 'User' } ];
 
-    this._rows = [] 
+    this._rows = []
 
     this.state = {
       followers: [],
@@ -41,36 +37,55 @@ class HomePage extends React.Component {
       fetchedFollowers: false,
       fetchedFollowing: false,
       followingSth: [],
-      latestFollower: 1
+      latestFollower: 1,
+      tkn: localStorage.getItem('tkn')
     }
     this.getCurrent = this.getCurrent.bind(this)
   }
 
+  onSuccess = (response) => {
+    console.log(response);
+    axios.post('http://localhost:8000/api/login', { code: response.code, accept: 'json' })
+      .then(result => {
+        console.log(result)
+        localStorage.setItem('tkn', result.data.tkn);
+        this.setState({ tkn: result.data.tkn });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  onFailure = (response) => {
+    console.error(response);
+  }
+
+
   getFollows = () => {
+    console.log(this.state.tkn);
     return axios.request({
       url: `https://api.github.com/users/hristo2612/followers?page=${this.state.pageNumber}`,
       method: 'get',
-      headers: { 'Authorization': 'token YOUR_AUTH_TOKEN_HERE' }
+      headers: { 'Authorization': `token ${this.state.tkn}` }
     })
   }
 
   createRows = () => {
-    let rows = [];
+    let followers = [];
     this.getFollows().then((res) => {
       console.log(res.data);
-      var followers = res.data;
-      if (followers.length < 1) alert('No more followers to load');
-      for (let i = 0; i < followers.length; i++) {
-        rows.push({
-          id: ordinal(i + this.state.latestFollower),
-          avatar: followers[i].avatar_url,
-          user: followers[i].login
-        });
+      var data = res.data;
+      if (data.length < 1) alert('No more data to load');
+      else {
+        for (let i = 0; i < data.length; i++) {
+          followers.push({
+            id: ordinal(i + this.state.latestFollower),
+            avatar: data[i].avatar_url,
+            user: data[i].login,
+            url: data[i].html_url
+          });
+        }
+        this.setState({ followers: [...this.state.followers, ...followers], pageNumber: this.state.pageNumber + 1, latestFollower: followers.length + this.state.latestFollower });
       }
-      
-      this._rows = [...this._rows, ...rows];
-      this.setState({followers: this._rows, pageNumber: this.state.pageNumber + 1, latestFollower: followers.length + this.state.latestFollower});
-      this.forceUpdate();
     }).catch((err) => {
       console.log(err);
     })
@@ -85,19 +100,25 @@ class HomePage extends React.Component {
   };
 
   render() {
+    let loginButton = null;
+    let followButton = null;
+    if (!this.state.tkn) {
+      loginButton = <GitHubLogin clientId="3145d9a7608514f31567"
+        className="styledButton"
+        redirectUri=""
+        scope="user"
+        onSuccess={this.onSuccess}
+        onFailure={this.onFailure} />;
+    } else {
+      followButton = <button className="styledButton" onClick={this.getCurrent}>
+        Load Followers
+      </button>;
+      loginButton = <button className="styledButton blue">Logged in..</button>;
+    }
     return (
       <div>
-        <h1>Home Page</h1>
-        <Button variant="raised" color="primary" onClick={this.getCurrent}>
-           Load More Followers
-        </Button>   
-        <Dialog/>  
-        {/* <Grid
-        columns={this._columns}
-        rowGetter={this.rowGetter}
-        rowsCount={this._rows.length}
-        defaultRow={{id: 1}}
-        minHeight={500} /> */}
+        {loginButton}
+        {followButton}
         <List followers={this.state.followers} style={leftListStyle} />
         <List followers={this.state.followers} style={rightListStyle} />
       </div>
