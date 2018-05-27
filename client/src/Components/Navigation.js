84986cba4a3ea67843bb4aaaee416d060b7d2e56
hristo2612 <hristoapps@gmail.com>
2018-05-27 22:34:26 +0300

    refactor: navigation

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,14 +1,28 @@
 // External Lib Imports
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class Navigation extends React.Component {
   render() {
     return (
       <div>
-        <Link className="button" to="/"><button>Home</button></Link>
-        <Link className="button" to="/followers"><button>Followers</button></Link>
-        <Link className="button" to="/following"><button>Following</button></Link>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Link className="button" to="/"><Button color="inherit">Home</Button></Link>
+            <Link className="button" to="/followers"><Button color="inherit">Followers</Button></Link>
+            <Link className="button" to="/following"><Button color="inherit">Following</Button></Link>
+            
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
